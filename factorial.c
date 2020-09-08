b8b11c863615fb98e0758bcb999b44a0a12e7cc1
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-08 20:55:06 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{int factorial();
+int result,num;
+printf("Enter the number:");
+scanf("%d",&num);
+result=factorial(num);
+printf("Factorial=%d",result);
+}
+
+int factorial(int n)
+{int total;
+if(n==0)
+    return 1;
+else
+    total=n*factorial(n-1);
+    return(total);
+}
